f57e26f6d7e5ace3864a2a8d7ad3032e568c7abe
Radoslaw Luczak <radoslaw.luczak.dev@gmail.com>
2019-04-11 15:35:05 +0200

    Admin Pages part.1

---
+++ Models/Data/DB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CmsShop.Models.Data
+{
+    public class Db : DbContext
+    {
+        public DbSet<PageDTO> Pages { get; set; }
+    }
+}
