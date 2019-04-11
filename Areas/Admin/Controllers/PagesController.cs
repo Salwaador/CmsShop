a8218083db4c906e5f33c283e0252824603970e8
Radoslaw Luczak <radoslaw.luczak.dev@gmail.com>
2019-04-11 16:08:20 +0200

    Admin Pages part.2

--- Areas/Admin/Controllers/PagesController.cs
+++ Areas/Admin/Controllers/PagesController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Pages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace CmsShop.Areas.Admin.Controllers
 {
@@ -7,7 +11,18 @@ namespace CmsShop.Areas.Admin.Controllers
         // GET: Admin/Pages
         public ActionResult Index()
         {
-            return View();
+            // Declaration of PageVM list
+            List<PageVM> pagesList;
+
+
+            using (Db db = new Db())
+            {
+                //List initial
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            //Return pages to view 
+            return View(pagesList);
         }
     }
 }
