7c189f1460966b63eb57d8ba40fd1ad58d9f2146
Radoslaw Luczak <radoslaw.luczak.dev@gmail.com>
2019-04-13 14:07:08 +0200

    Admin Pages part.4

--- Areas/Admin/Controllers/PagesController.cs
+++ Areas/Admin/Controllers/PagesController.cs
@@ -26,9 +26,61 @@ namespace CmsShop.Areas.Admin.Controllers
         }
 
         // GET: Admin/Pages/AddPage
+        [HttpGet]
         public ActionResult AddPage()
         {
             return View();
+        }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //Check state model
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                string slug;
+
+                //PageDTO initialization
+                PageDTO dto = new PageDTO();
+
+                //If we dont have page address, we assign title
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                //prevent to add the same name of page
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "Ten tytuł, lub adres strony już istnieje!");
+
+                    return View(model);
+                }
+
+                dto.Title = model.Title;
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 1000;
+
+                //DTO save
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "Dodałeś nową stronę";
+
+            return RedirectToAction("AddPage"); 
         }
     }
 }
